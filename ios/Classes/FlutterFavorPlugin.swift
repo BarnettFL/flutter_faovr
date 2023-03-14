f79bda617a1a93817bfa364b49ed58ce4ed5798c
Barnett <Barnett@favorlabs.io>
2023-03-14 13:38:13 +0800

    feat: add favorX for ios

--- ios/Classes/FlutterFavorPlugin.swift
+++ ios/Classes/FlutterFavorPlugin.swift
@@ -1,7 +1,10 @@
 import Flutter
 import UIKit
+import FavorX
 
 public class FlutterFavorPlugin: NSObject, FlutterPlugin {
+  var node : FavorX.MobileNode?;
+  
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "flutter_favor", binaryMessenger: registrar.messenger())
     let instance = FlutterFavorPlugin()
@@ -9,6 +12,207 @@ public class FlutterFavorPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    switch call.method {
+    case "version":
+      result(FavorX.MobileVersion())
+      break
+    case "start":
+      let s = call.arguments as! String
+      let jsonData = s.data(using: .utf8)!
+      
+      var optionsObj :Options!
+      do {
+          let decoder = JSONDecoder()
+          optionsObj = try decoder.decode(Options.self, from: jsonData)
+      } catch  {
+          result(error)
+          break
+      }
+      result(self.start(o: optionsObj))
+      break
+    case "stop":
+      self.stop(result: result)
+      break
+    default:
+      result(FlutterMethodNotImplemented)
+    }
+  }
+  
+  private func stop(result: FlutterResult) {
+      if node != nil {
+          do {
+              try node!.stop()
+          } catch {
+              result(error)
+              return
+          }
+      }
+      result(nil)
+  }
+  
+  struct Group: Decodable {
+      let name: String
+      let type: Int
+      let keepConnectedPeers: Int
+      let nodes: Array<String>
+      
+      enum CodingKeys: String, CodingKey {
+          case name = "name"
+          case type = "type"
+          case keepConnectedPeers = "keep-connected-peers"
+          case nodes = "nodes"
+      }
+      
+      init(from decoder: Decoder) throws {
+              let container = try decoder.container(keyedBy: CodingKeys.self)
+          nodes = try container.decode(Array<String>.self, forKey: .nodes)
+          name = try container.decode(String.self, forKey: .name)
+          type = try container.decode(Int.self, forKey: .type)
+          keepConnectedPeers = try container.decode(Int.self, forKey: .keepConnectedPeers)
+      }
+  }
+  
+  struct Options: Decodable {
+      let apiPort: Int
+      let debugApiPort: Int
+      let wsPort: Int
+      let debugApiEnable: Bool
+      let proxyEnable: Bool
+      let proxyGroup: String
+      let proxyPort: Int
+      let groups: Array<Group>
+      let networkId: Int64
+      let p2pPort: Int
+      let welcomeMsg: String
+      let binMaxPeers: Int
+      let lightMaxPeers: Int
+      let cacheCapacity: Int64
+      let bootNodes: String
+      let devMode: Bool
+      let fullNode: Bool
+      let chainEndpoint: String
+      let oracleContractAddr: String
+      let traffic: Bool
+      let trafficContractAddr: String
+      let verbosity: String
+      let enableTLS: Bool
+      let password: String
+      let dataPath: String
+      
+      enum CodingKeys: String, CodingKey {
+          case apiPort = "api-port"
+          case debugApiPort = "debug-api-port"
+          case wsPort = "ws-port"
+          case debugApiEnable = "debug-api-enable"
+          case proxyEnable = "proxy-enable"
+          case proxyGroup = "proxy-group"
+          case proxyPort = "proxy-port"
+          case groups = "groups"
+          case networkId = "network-id"
+          case p2pPort = "p2p-port"
+          case welcomeMsg = "welcome-message"
+          case binMaxPeers = "bin-max-peers"
+          case lightMaxPeers = "light-max-peers"
+          case cacheCapacity = "cache-capacity"
+          case bootNodes = "boot-nodes"
+          case devMode = "dev-mode"
+          case fullNode = "full-node"
+          case chainEndpoint = "chain-endpoint"
+          case oracleContractAddr = "oracle-contract-addr"
+          case traffic = "traffic"
+          case trafficContractAddr = "traffic-contract-addr"
+          case verbosity = "verbosity"
+          case enableTLS = "enable-tls"
+          case password = "password"
+          case dataPath = "data-dir"
+      }
+      
+      init(from decoder: Decoder) throws {
+              let container = try decoder.container(keyedBy: CodingKeys.self)
+          apiPort = try container.decode(Int.self, forKey: .apiPort)
+          debugApiPort = try container.decode(Int.self, forKey: .debugApiPort)
+          wsPort = try container.decode(Int.self, forKey: .wsPort)
+          debugApiEnable = try container.decode(Bool.self, forKey: .debugApiEnable)
+          proxyEnable = try container.decode(Bool.self, forKey: .proxyEnable)
+          proxyGroup = try container.decode(String.self, forKey: .proxyGroup)
+          proxyPort = try container.decode(Int.self, forKey: .proxyPort)
+          groups = try container.decode(Array<Group>.self, forKey: .groups)
+          networkId =  try container.decode(Int64.self, forKey: .networkId)
+          p2pPort =  try container.decode(Int.self, forKey: .p2pPort)
+          welcomeMsg =  try container.decode(String.self, forKey: .welcomeMsg)
+          binMaxPeers =  try container.decode(Int.self, forKey: .binMaxPeers)
+          lightMaxPeers =  try container.decode(Int.self, forKey: .lightMaxPeers)
+          cacheCapacity =  try container.decode(Int64.self, forKey: .cacheCapacity)
+          bootNodes =  try container.decode(String.self, forKey: .bootNodes)
+          devMode =  try container.decode(Bool.self, forKey: .devMode)
+          fullNode =  try container.decode(Bool.self, forKey: .fullNode)
+          chainEndpoint = try container.decode(String.self, forKey: .chainEndpoint)
+          oracleContractAddr = try container.decode(String.self, forKey: .oracleContractAddr)
+          traffic = try container.decode(Bool.self, forKey: .traffic)
+          trafficContractAddr = try container.decode(String.self, forKey: .trafficContractAddr)
+          verbosity = try container.decode(String.self, forKey: .verbosity)
+          enableTLS = try container.decode(Bool.self, forKey: .enableTLS)
+          password = try container.decode(String.self, forKey: .password)
+          dataPath = try container.decode(String.self, forKey: .dataPath)
+      }
+  }
+  
+
+  private func start(o: Options) -> NSError? {
+      let options = MobileOptions()
+      // api setting
+      options.apiPort = o.apiPort
+      options.debugAPIPort = o.debugApiPort
+      options.websocketPort = o.wsPort
+      options.enableDebugAPI = o.debugApiEnable
+      
+      // proxy setting
+      options.proxyEnable = o.proxyEnable
+      options.proxyGroupName = o.proxyGroup
+      options.proxyPort = o.proxyPort
+      
+      // group setting
+      let data: Data = try! JSONSerialization.data(withJSONObject: o.groups)
+      options.group = String(data: data, encoding: String.Encoding.utf8)!
+      
+      // p2p setup
+      options.networkID = o.networkId
+      options.p2PPort = o.p2pPort
+      options.welcomeMessage = o.welcomeMsg
+      
+      // kademlia
+      options.binMaxPeers = o.binMaxPeers
+      options.lightMaxPeers = o.lightMaxPeers
+      
+      // cache size
+      options.cacheCapacity = o.cacheCapacity
+      
+      // node bootstrap
+      options.bootNodes = o.bootNodes
+      options.enableDevNode = o.devMode
+      options.enableFullNode = o.fullNode
+      
+      // chain setting
+      options.chainEndpoint = o.chainEndpoint
+      options.oracleContract = o.oracleContractAddr
+      
+      // traffic stat
+      options.enableFlowStat = o.traffic
+      options.flowContract = o.trafficContractAddr
+      
+      // security
+      options.password = o.password
+      options.dataPath = o.dataPath
+      
+      // misc
+      options.verbosity = o.verbosity
+      options.enableTLS = o.enableTLS
+      
+      var error: NSError?
+      node = FavorX.MobileNewNode(options, &error)
+      if error != nil {
+          return error
+      }
+      return nil
   }
 }
--- ios/flutter_favor.podspec
+++ ios/flutter_favor.podspec
@@ -4,10 +4,10 @@
 #
 Pod::Spec.new do |s|
   s.name             = 'flutter_favor'
-  s.version          = '0.0.1'
-  s.summary          = 'A new Flutter plugin project.'
+  s.version          = '1.4.1'
+  s.summary          = 'A favorX Flutter plugin project.'
   s.description      = <<-DESC
-A new Flutter plugin project.
+A favorX Flutter plugin project.
                        DESC
   s.homepage         = 'http://example.com'
   s.license          = { :file => '../LICENSE' }
@@ -15,9 +15,11 @@ A new Flutter plugin project.
   s.source           = { :path => '.' }
   s.source_files = 'Classes/**/*'
   s.dependency 'Flutter'
-  s.platform = :ios, '9.0'
+  s.platform = :ios, '15.3'
 
   # Flutter.framework does not contain a i386 slice.
   s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES', 'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'i386' }
   s.swift_version = '5.0'
+  s.vendored_frameworks = 'Framework/favorX.xcframework'
+  s.static_framework = true
 end
